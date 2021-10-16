c9cc3f1208595c520330fc92e64cf768c9ad122a
nicksorenson <nicksorenson99@gmail.com>
2021-10-15 20:57:19 -0600

    First commit

---
+++ hw06/hw06/PyCYK/cyk.py
@@ -0,0 +1,13 @@
+###############################################
+# module: cyk.py
+# YOUR NAME
+# YOUR A-#
+###############################################
+
+class CYK(object):
+
+    @staticmethod
+    def is_in_cfl(test_string, cnfg):
+        ### your code here
+        pass
+
