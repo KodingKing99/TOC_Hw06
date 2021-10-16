7e8da0862ba68d35bd287bb373bffc477f5e351a
nicksorenson <nicksorenson99@gmail.com>
2021-10-16 13:32:19 -0600

    First commit. I'm close but the 0 and 1 indexing is messing me up. Gona get some food

--- hw06/hw06/PyCYK/cyk.py
+++ hw06/hw06/PyCYK/cyk.py
@@ -8,6 +8,26 @@ class CYK(object):
 
     @staticmethod
     def is_in_cfl(test_string, cnfg):
-        ### your code here
+        print("Hello World!")
+        print(test_string)
+        print("printing cnfg")
+        print(cnfg)
+        print(f"Test String Length: {len(test_string)}")
+        # Create Table
+        D = [[0 for i in range(len(test_string))] for n in range(1, len(test_string))]
+        print(D)
+        # Initialize the table, D[s,l] -> D[i, 0] x at s length of 1.
+        # for i in range(len(test_string)):
+        #     D[i][0] = (cnfg.fetch_lhs(test_string[i]))
+        # print(D)
+        # print("Doing table stuff")
+        # for l in range(1, len(test_string)):
+        #     for s in range(0, len(test_string) - l):
+        #         for k in range(0, l):
+        #             print(f"D[{s}][{l}] = D[{s}][{k}] X D[{s + k}][{l - k}]")
+
+                
+            # d[i][]
+
         pass
 
